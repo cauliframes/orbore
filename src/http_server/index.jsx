7100f3ce84d37e7ee6411682f71ea1a7891f4001
iqbal <cauliframes@gmail.com>
2024-02-10 14:55:17 +0700

    jsx tested

--- src/http_server/index.jsx
+++ src/http_server/index.jsx
@@ -1,17 +1,49 @@
 import Koa from 'koa'
 import Router from 'koa-router'
+import conditional from 'koa-conditional-get'
+import etag from 'koa-etag'
+import React, { isValidElement } from 'react'
+import { renderToStaticMarkup } from 'react-dom/server';
+const jsx = () => async (ctx, next) => {
+  await next()
+
+  if (isValidElement(ctx.response.body)) {
+    ctx.response.body = [
+      '<!DOCTYPE html>',
+      renderToStaticMarkup(ctx.response.body),
+    ].join('\n')
+    ctx.response.type = 'text/html; charset=utf-8'
+  }
+}
+
 
 function HttpServer() {
   const koa = new Koa()
   const router = new Router()
 
-  router.get('/', (ctx, _) => {
-    ctx.response.status = 200
+  let server = { close: () => { } }
+  const requestSystemInitiation = async () => {
+    server = koa.listen(19000)
+    console.log('http_server is listening')
+  }
+
+  const requestSystemTermination = async () => {
+    console.log('http_server is closing')
+    server.close()
+  }
+
+  koa.use(conditional())
+  koa.use(etag())
+  koa.use(jsx())
+
+  router.get('/', async (ctx, _) => {
+    ctx.response.body = <h3>
+      skrrt
+    </h3>
   })
 
-  let server = { close: () => { } }
-  router.get('/termination', (ctx, _) => {
-    server.close()
+  router.get('/termination', async (ctx, _) => {
+    requestSystemTermination()
     ctx.response.status = 200
   })
 
@@ -19,15 +51,8 @@ function HttpServer() {
   koa.use(router.allowedMethods())
 
   return {
-    requestSystemInitiation: async () => {
-      server = koa.listen(19000)
-      console.log('http_server is listening')
-    },
-
-    requestSystemTermination: async () => {
-      console.log('http_server is closing')
-      server.close()  
-    }
+    requestSystemInitiation,
+    requestSystemTermination,
   }
 }
 
